e0abfd6453739959ae1bd706091ecef48a3e1cd7
anangpermana <anang143@gmail.com>
2024-12-04 18:23:16 +0700

    add cors

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fileExplorerRoutes = require('./routes/fileExplorerRoutes');
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://your-domain.com'],
+}));
 app.use(bodyParser.json());
 app.use('/api', fileExplorerRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
